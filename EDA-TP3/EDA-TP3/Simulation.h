8ded7b993062096bac85327ca1e58414971bf88d
Ezequiel Vijande <evijande@itba.edu.ar>
2017-08-25 19:36:53 -0300

    Definciones generales

---
+++ EDA-TP3/EDA-TP3/Simulation.h
@@ -0,0 +1,34 @@
+#ifndef SIMULATION_H
+
+#include "Structures.h"
+
+robot_t CreateRobots(unsigned int Number, unsigned int height, unsigned width);
+//Crea un heap con el numero de robots a crear y les da un valor incial aleatorio
+//de posicision y de angulo a cada uno.
+
+void MoveRobot(robot_t *robot, unsigned int height, unsigned int width);
+//Funcion que mueve un robot una unidad en la direccion correspondiente.
+
+pos_t GetRobotPos(const robot_t *robot);
+//Devuelve las coordenadas de un robot determinado.
+
+void DestroyRobots(robot_t *robots);
+//Libera la memoria usada por los robots.
+
+piso_t* CreateFloor(unsigned int width, unsigned int height);
+//Crea la memoria para el piso e inicializa todas las baldosas
+//como sucias.
+
+sim_t* CreateSim(unsigned int RobotCount, unsigned int height, unsigned int width);
+//Crea y guarda todos los datos necesarios para correr la simulacion.
+
+unsigned long RunSim(sim_t* simulation);
+//Corre la simulacion hasta limpiar todas las baldosas y devuelve
+//el numero de ticks que tardo en finalizar.
+
+void DestroySim(sim_t* simulation);
+//Lbera la memoria creada por la simulacion.
+
+
+#endif // !SIMULATION_H
+
---
+++ EDA-TP3/EDA-TP3/Structures.h
@@ -0,0 +1,40 @@
+//Modulo en el que se definen las estructuras generales usadas
+//en el programa.
+
+#ifndef DEFINTIONS_H
+
+#define SUCIO false
+#define LIMPIO true
+
+typedef struct
+{
+	double x;
+	double y;
+}pos_t;
+
+typedef struct
+{
+	pos_t pos;
+	double angle;
+
+}robot_t;
+
+typedef struct
+{
+	bool state;
+}piso_t;
+
+
+//Estructura en la que se guarda toda la informacion necesaria
+//para correr la simulacion..
+typedef struct 
+{
+	robot_t* robots;
+	unsigned int robot_count;
+	piso_t* piso;
+	unsigned int width;
+	unsigned int height;
+	unsigned long tick_count;
+
+}sim_t;
+#endif // !DEFINTIONS_H
