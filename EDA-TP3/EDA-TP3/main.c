4844dcbb124c3577c404bf84a3f3a1b94311b114
Ezequiel Vijande <evijande@itba.edu.ar>
2017-08-31 00:04:01 -0300

    no anda modo 2

--- EDA-TP3/EDA-TP3/main.c
+++ EDA-TP3/EDA-TP3/main.c
@@ -18,9 +18,10 @@ int main(int argc, char* argv[])
 {
 	ALLEGRO_DISPLAY *display = NULL;
 	imagenes_t* imagenes = NULL;
+	char mode = '2';
 	unsigned long Tick_counter = 0;
-	unsigned int width = 5;
-	unsigned int height = 5;
+	unsigned int width = 30;
+	unsigned int height = 30;
 	unsigned int n_robots = 1;
 	srand(time(NULL));
 
@@ -45,92 +46,96 @@ int main(int argc, char* argv[])
 	
 	
 	
-	
-	//Caso1
-
-	display = al_create_display(width * UNIT, height * UNIT);
-	if (display == NULL)
+	if(mode=='1')
 	{
-	fprintf(stderr, "failed to create display!\n");
-	return ERROR;
+
+		display = al_create_display(width * UNIT, height * UNIT);
+		if (display == NULL)
+		{
+		fprintf(stderr, "failed to create display!\n");
+		return ERROR;
+		}
+		sim_t* Sim= CreateSim(n_robots, height, width);
+		if (Sim == NULL)
+		{
+		al_destroy_display(display);
+		DestroyImages(imagenes);
+		return -1;
+
+		}
+		ActualizarDisplay(Sim, imagenes);
+		Tick_counter= RunSim1(Sim, imagenes);
+		fprintf(stdout, "\nTicks: %ul\n", Tick_counter);
+		al_destroy_display(display);
+		al_rest(2);
+		DestroySim(Sim);
+		DestroyImages(imagenes);
+
+
+		return 0;
 	}
-	sim_t* Sim= CreateSim(n_robots, height, width);
-	if (Sim == NULL)
-	{
-	al_destroy_display(display);
-	DestroyImages(imagenes);
-	return -1;
-
-	}
-	ActualizarDisplay(Sim, imagenes);
-	Tick_counter= RunSim1(Sim, imagenes);
-	fprintf(stdout, "\nTicks: %ul\n", Tick_counter);
-	al_destroy_display(display);
-	al_rest(2);
-	DestroySim(Sim);
-	DestroyImages(imagenes);
-
-
-	return 0;
 
 
 	//Caso2
-/*
-	unsigned long Tick[100];
-	sim_t* Sim= NULL;
+	else if (mode == '2')
+	{
+		unsigned long Tick[100];
+		sim_t* Sim = NULL;
+		
 
-	display = al_create_display(1080,900);
-	if (display == NULL)
-	{
-		fprintf(stderr, "failed to create display!\n");
-		return ERROR;
-	}
-	Tick[0] = 0;
-	for (int i = 0; i < MAX_ITERATIONS; i++)
-	{
-		Sim = CreateSim(1, height, width);
-		if (Sim == NULL)
+		display = al_create_display(1080, 900);
+		if (display == NULL)
 		{
-			al_destroy_display(display);
+			fprintf(stderr, "failed to create display!\n");
 			return ERROR;
 		}
-		printf("\nHola%d\n", i);
-		Tick[0]+=RunSim2(Sim);
-		printf("\nHola%d\n", i);
-
-		DestroySim(Sim);
-
-	}
-	Tick[0] /= MAX_ITERATIONS;
-	unsigned int j = 1;
-	printf("\nHola\n");
-	do
-	{
-		Tick[j] = 0;
-		Sim = CreateSim(j+1, height, width);
-		if (Sim == NULL)
-		{
-			al_destroy_display(display);
-			return ERROR;
-		}
-
+		Tick[0] = 0;
 		for (int i = 0; i < MAX_ITERATIONS; i++)
 		{
-			Tick[j] +=  RunSim2(Sim);
+			Sim = CreateSim(1, height, width);
+			if (Sim == NULL)
+			{
+				al_destroy_display(display);
+				return ERROR;
+			}
+			//printf("\nHola%d\n", i);
+			Tick[0] += RunSim2(Sim);
+			//printf("\nHola%d\n", i);
+
+			DestroySim(Sim);
+
 		}
-		Tick[j] /= MAX_ITERATIONS;
-		DestroySim(Sim);
-		j++;
-	}while (((Tick[j-2]) - (Tick[j-1])) > 4);
+		Tick[0] /= MAX_ITERATIONS;
+		unsigned int j = 1;
+		printf("\nHola\n");
+		do
+		{
+			Tick[j] = 0;
+			Sim = CreateSim(j + 1, height, width);
+			if (Sim == NULL)
+			{
+				al_destroy_display(display);
+				return ERROR;
+			}
+
+			for (int i = 0; i < MAX_ITERATIONS; i++)
+			{
+				Tick[j] += RunSim2(Sim);
+			}
+			Tick[j] /= MAX_ITERATIONS;
+			DestroySim(Sim);
+			j++;
+		} while (((Tick[j - 2]) - (Tick[j - 1])) > 4);
+
+		PrintHistogram(j, display, Tick);
+		al_flip_display();
+		al_rest(6);
+		al_destroy_display(display);
+
+
+		return 0;
+	}
 	
-	PrintHistogram(j, display, Tick);
-	al_flip_display();
-	al_rest(6);
-	al_destroy_display(display);
-
-
-	return 0;
-	*/
 }
 
 int Initialization(void)
